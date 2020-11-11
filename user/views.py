59be679b8879e180255bedc3915086c04330c016
GQA9517 <1131529581@qq.com>
2020-11-11 20:23:32 +0800

    222

--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
 # Create your views here.
+
+
+
+def test():
+    print(111)
+    print(222)
+    return HttpResponse('测试')
+
